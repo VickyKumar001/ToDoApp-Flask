9b4c973677578b41a229c06e06e572337033e053
VickyKumar001 <vk7434075@gmail.com>
2025-06-27 12:21:38 +0530

    add port

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
 from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+import pytz
+import os
+
+IST = pytz.timezone('Asia/Kolkata')
+
+def get_ist_time():
+    return datetime.now(IST)
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///todo.db"
@@ -12,7 +19,7 @@ class Todo(db.Model):
     sno = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(200), nullable=False)
     desc = db.Column(db.String(500), nullable=False)
-    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+    date_created = db.Column(db.DateTime, default=get_ist_time)
 
     def __repr__(self) -> str:
         return f"{self.sno} - {self.title}"
@@ -69,5 +76,5 @@ def delete(sno):
     return redirect("/")
 
 if __name__ == '__main__':
-     # app.run(debug=True, port=8001) # For development
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Get PORT from env or use 5000
+    app.run(debug=True, host='0.0.0.0', port=port)
